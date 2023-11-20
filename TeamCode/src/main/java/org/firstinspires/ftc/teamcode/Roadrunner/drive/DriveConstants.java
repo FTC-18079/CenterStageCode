c9ad1e1ac1d7f24a48c815344d2d6cc20fc00c78
Nicolas <nicsilvc@gmail.com>
2023-11-19 17:57:12 -0800

    Changed TICKS_PER_REV in Localizer and WHEEL_RADIUS in DriveConstants. Merged in changes from claw rework

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
@@ -44,7 +44,7 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 1.88976378; // in
+    public static double WHEEL_RADIUS = 0.94488189; // in inches; 24 mm
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 14.55; // in
 
